134dc23616630a63d5e039a14d3b8eaf63b1b1b3
Raushan kumar <119419311+Raushankumar-prog@users.noreply.github.com>
2024-05-15 09:32:35 +0530

    Time: 4 ms (58.49%), Space: 14.7 MB (14.99%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+           ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+       int n = nums.size();
+        if (n <= 1) return;
+
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+
+        
+        reverse(nums.begin() + i + 1, nums.end());
+        
+    }
+};
