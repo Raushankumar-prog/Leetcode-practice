c79f374e2c796590085c12b8076dbc53e9dcde0f
Raushan kumar <119419311+Raushankumar-prog@users.noreply.github.com>
2024-06-06 09:58:14 +0530

    Time: 0 ms (100%), Space: 8.1 MB (44.32%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+          ios_base::sync_with_stdio(false);
+            cin.tie(NULL);
+
          long long N = n;
         if (N < 0) {
             x = 1 / x;
