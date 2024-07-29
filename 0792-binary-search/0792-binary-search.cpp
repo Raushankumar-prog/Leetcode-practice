e02c9d5cf0ed801113262bcbffa556b66602f0a3
Raushan kumar <raushankumar8757825@gmail.com>
2024-07-29 07:19:13 +0530

    Time: 20 ms (92.6%), Space: 30.2 MB (21.58%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,24 +1,21 @@
 class Solution {
+
+public:
+int binary_search(vector<int>&nums,int low, int high,int target){   
+       if (low > high) return -1;  
+        int mid=(low+high)/2;
+        if(nums[mid]==target) return mid;
+        else if(target<nums[mid]) return binary_search(nums,low,mid-1,target);
+        else return binary_search(nums,mid+1,high,target);
+
+}
+
 public:
     int search(vector<int>& nums, int target) {
-        ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int n=nums.size();
-    int low=0;
-    int high=n-1;
-    while(low<=high){
-        int mid=(low+high)/2;
-        if(nums[mid]==target){
-            return mid;
-        }
-        else if(nums[mid]>target){
-            high=mid-1;
-        }
-        else{
-            low=mid+1;
-        }
-    }
+        int low=0;
+        int high=nums.size()-1;
+        return  binary_search(nums,low,high,target);
+    
+}
 
-        return -1;
-    }
 };
